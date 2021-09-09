fcb8ffe1411807b7c777a1d6881617259509b2d5
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-09-09 10:21:31 -0500

    Initial Commit

---
+++ Simon Game/Simon_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simon_GameApp.swift
+//  Simon Game
+//
+//  Created by Student on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Simon_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
